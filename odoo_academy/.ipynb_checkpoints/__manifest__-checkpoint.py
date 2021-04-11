e99844f7e7f3c41e050d1655058fe94cc1df8aea
joeleetorres <jtorres@prcomputer.net>
2021-04-11 18:40:30 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,24 @@
+# _*_ coding: utf-8  _*_
+
+{
+    'name': 'Odoo Virtual Academy - PRCS Campus',
+    'summary': """Academy to manage PRCS Training""",
+    'description': """
+        Academy Module to manage PRCS Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    'Author': 'PR Computer Svcs',
+    'website': 'http://www.prcomputer.com',
+    'category': 'PRCS Training',
+    'depends': ['base'],
+    'data:': [
+        
+        
+    ], 
+    
+    'demo': [
+     
+    ], 
+}
